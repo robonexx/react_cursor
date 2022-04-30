76b3fea7bab39ea6c338f2ff4fa0017a932d98ef
robonexx <robertwagar@gmail.com>
2022-04-30 09:57:30 +0200

    adding some elements to try out the hover effect, want to make it bigger on hover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 
 const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -7,6 +7,7 @@ const Cursor = () => {
   useEffect(() => {
     addEventListeners();
     return () => removeEventListeners();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const addEventListeners = () => {
@@ -24,15 +25,15 @@ const Cursor = () => {
   return (
     <div
       className='cursor'
-      style={{
+      /* style={{
         left: `${position.x}px`,
         top: `${position.y}px`,
-      }}
+      }} */
     ></div>
   );
 };
 
-function App({cursor}) {
+function App({ cursor }) {
   return (
     <>
       <Cursor
@@ -43,7 +44,27 @@ function App({cursor}) {
           cursor.style.top = `${e.pageY}px`;
         }}
       />
-      <div className='App'></div>
+      <div className='App'>
+        <div className='page_top'>
+          <h1>[kɹiːˌe͡ɪtˈɪvɪti]</h1>
+        </div>
+
+        <div className='content_wrapper'>
+          <div className='left_side'>
+            <h2>Custom cursor invert on hover</h2>
+          </div>
+          <div className='right_side'>
+            <h4>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+              repudiandae voluptatum debitis, explicabo sit laboriosam molestias
+              vel rerum dicta doloribus incidunt obcaecati ex aliquid
+              necessitatibus nemo, iure iste! Esse, illo, rem a inventore nobis
+              consequuntur corrupti totam vel nesciunt saepe adipisci alias qui
+              velit laborum. Exercitationem amet quidem excepturi quas.
+            </h4>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
